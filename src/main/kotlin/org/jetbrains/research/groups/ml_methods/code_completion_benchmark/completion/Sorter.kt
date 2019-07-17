4d64a58ef82a8d6456eadc9302ed26a4c9da7af3
Anastasia Tuchina <anastasia.i.tuchina@gmail.com>
2019-07-17 17:15:39 +0300

    Create custom completion sorter service

---
+++ src/main/kotlin/org/jetbrains/research/groups/ml_methods/code_completion_benchmark/completion/Sorter.kt
@@ -0,0 +1,29 @@
+package org.jetbrains.research.groups.ml_methods.code_completion_benchmark.completion
+
+import com.intellij.codeInsight.completion.CompletionFinalSorter
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.lookup.LookupElement
+
+import com.intellij.openapi.util.Pair
+
+@Suppress("DEPRECATION")
+class CompletionSorterFactory : CompletionFinalSorter.Factory {
+    override fun newSorter() = Sorter()
+}
+
+class Sorter : CompletionFinalSorter() {
+
+    private fun rankItems(items: MutableIterable<LookupElement>): MutableIterable<LookupElement> {
+        //TODO: implement ranking
+        return items
+    }
+
+    override fun sort(items: MutableIterable<LookupElement>, parameters: CompletionParameters): MutableIterable<LookupElement> {
+        val sorted = rankItems(items)
+        return sorted
+    }
+
+    override fun getRelevanceObjects(elements: MutableIterable<LookupElement>): Map<LookupElement, MutableList<Pair<String, Any>>> {
+        return elements.associateWith { listOf(Pair.create("", "" as Any)).toMutableList() }
+    }
+}
